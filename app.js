17517e4d6f82d82547c8d17c65560b85744bb215
Derien <haniaroadlifestyle@gmail.comgit config --listgit config --listgit config --global user.name = Deriengit config --global user.email = haniaroadlifestyle@gmail.com>
2019-05-27 15:44:45 -0400

    Created sliding sign up and sign in animation

---
+++ app.js
@@ -0,0 +1,15 @@
+const signup = document.getElementById("signup");
+const signin = document.getElementById("signin");
+const container = document.getElementById("container");
+
+signup.addEventListener('click', () => {
+  
+
+    container.classList.add('right-panel-active');
+});
+
+signin.addEventListener('click', () => {
+   
+
+    container.classList.remove('right-panel-active');
+});
